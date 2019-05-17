92541bfef82bd8874b0cc90fd68832439d3ed8e5
Michael Karrer <michaelkarrer81@gmail.com>
2019-05-17 19:06:15 +0200

    [ADD] __openerp__.py for the default instance addon

---
+++ addons/veme_config/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'veme_config',
+    'summary': '''FS-Online veme instance configuration''',
+    'description': '''
+FS-Online Instance Configuration
+================================
+
+Customer configuration for the instance veme
+
+- Default settings
+- View modifications
+- CSS
+- Translations
+    ''',
+    'author': 'Michael Karrer (michael.karrer@datadialog.net), DataDialog',
+    'version': '1.0',
+    'website': 'https://www.datadialog.net',
+    'installable': True,
+    'depends': [
+        'fsonline',
+    ],
+    'data': [],
+}
+
+    
